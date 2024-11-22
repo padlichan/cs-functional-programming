bfc4f6347c17dc0df3fdf67bbfcc03b278392cba
Lili <lili.berenyi@gmail.com>
2024-11-22 10:38:12 +0000

    Add empty project

---
+++ cs-functional-programming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cs_functional_programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
